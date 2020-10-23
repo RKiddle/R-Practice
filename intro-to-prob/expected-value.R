b343c34a74e91265dcd6ecd8f15541d383bc232e
Dr Richard Kiddle <RKiddle@users.noreply.github.com>
2020-10-23 15:34:42 +0700

    Update expected-value.R

--- intro-to-prob/expected-value.R
+++ intro-to-prob/expected-value.R
@@ -4,3 +4,11 @@
 
 # Confirm with a simulation using rbinom
 mean(rbinom(10000, 25, .3))
+
+
+# Calculate the variance using the exact formula
+# var = size * p * (1-p)
+
+25*.3*.7
+# Confirm with a simulation using rbinom
+var(rbinom(10000, 25, .3))
